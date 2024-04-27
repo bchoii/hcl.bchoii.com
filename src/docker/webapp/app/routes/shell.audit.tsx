18c91bb5917c428f43d0fbad7382aa1e73319929
Bernard Choi <bchoii@gmail.com>
2024-04-27 16:14:08 +0800

    add pagination

--- src/docker/webapp/app/routes/shell.audit.tsx
+++ src/docker/webapp/app/routes/shell.audit.tsx
@@ -6,17 +6,32 @@ import {
 } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const findAll = await fetch(
-    "https://a7ed-116-86-52-194.ngrok-free.app/v1/api/pms/audit/findAll"
-  ).then((r) => r.json());
+// http://localhost:5173/shell/audit?pageNumber=1&pageSize=15
+// http://localhost:5173/shell/audit
+
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const { searchParams } = new URL(request.url);
+  console.log("searchParams", searchParams);
+  console.log(searchParams.toString());
+
+  const pageNumber = searchParams.get("pageNumber");
+  const pageSize = searchParams.get("pageSize");
+  console.log("pageNumber", pageNumber);
+  console.log("pageSize", pageSize);
+
+  const url =
+    "https://a7ed-116-86-52-194.ngrok-free.app/v1/api/pms/audit/findAll?" +
+    searchParams.toString();
+
+  const findAll = await fetch(url).then((r) => r.json());
+  console.log("findAll", findAll);
   // invariant(params.id, "Missing id param");
   // return json({ id: params.id });
-  return { findAll };
+  return { url, findAll };
 };
 
 export default function Component() {
-  const { findAll } = useLoaderData<typeof loader>();
+  const { url, findAll } = useLoaderData<typeof loader>();
 
   return (
     <div className="fadein">
@@ -30,16 +45,38 @@ export default function Component() {
           <th>Trade Type</th>
           <th>Audit Date</th>
         </tr>
-        <tr>
-          <td></td>
-          <td>Placeholder</td>
-          <td>Placeholder</td>
-          <td>Placeholder</td>
-          <td>Buy / Sell</td>
-          <td>Placeholder</td>
-        </tr>
+        {findAll?.content?.map((r) => (
+          <tr>
+            <td></td>
+            <td>{r.transactionRef}</td>
+            <td>{r.instrumentId}</td>
+            <td>{r.instrumentName}</td>
+            <td>{r.tradeType}</td>
+            <td>{r.auditDate}</td>
+          </tr>
+        ))}
       </table>
-      <pre>{JSON.stringify(findAll, null, 2)}</pre>
+      <div>
+        {!findAll.first && (
+          <a
+            href={`?pageSize=${findAll.pageable.pageSize ?? 1}&pageNumber=${
+              findAll.pageable.pageNumber ?? 1
+            }`}
+          >
+            Prev
+          </a>
+        )}{" "}
+        {!findAll.last && (
+          <a
+            href={`?pageSize=${findAll.pageable.pageSize ?? 1}&pageNumber=${
+              (findAll.pageable.pageNumber ?? 1) + 2
+            }`}
+          >
+            Next
+          </a>
+        )}
+      </div>
+      <pre>{JSON.stringify({ url, findAll }, null, 2)}</pre>
     </div>
   );
 }
